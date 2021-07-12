89d3d522e242e19cd084d779b024fee2da173c44
leolermav <LeoLermaV@gmail.com>
2021-07-12 15:26:59 +1200

    added template route to inser into db

--- server.js
+++ server.js
@@ -6,6 +6,33 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post("/createweeklyassigner", (req, res) => {
+    /*/ 
+    props from website to db
+    db.query
+
+    ///template///
+    const user = req.body.user;
+    const pass = req.body.pass;
+    
+    db.query(
+        "INSERT INTO `users` (`username`, `name`,) VALUES (?,?);",
+        [user, pass],
+        (err, result) => {
+            if(err) {
+                console.log(err)
+            } else {
+                res.send("inserted")
+            }
+        }
+    );
+    ///template///
+
+    /*/
+});
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
